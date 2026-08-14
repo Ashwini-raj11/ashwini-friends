b814442575e3610519ccbe084a871288cdf7c1ea
Ashwini R <dranr427@gmail.com>
2026-08-14 20:16:29 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -15,3 +15,18 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcyycALWJZ1pRqsL4M90lfIibnrjsC5gQ",
+  authDomain: "ashwini-friends.firebaseapp.com",
+  projectId: "ashwini-friends",
+  storageBucket: "ashwini-friends.firebasestorage.app",
+  messagingSenderId: "50308492533",
+  appId: "1:50308492533:web:a0bbb1ab0976154bbf69bc"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
